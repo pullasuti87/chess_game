7ba399c1ca1f97260476913cacb2c19a5ee6a455
pullasuti87 <pullasuti87@gmail.com>
2025-02-24 12:47:57 +0000

    board_done

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+def init_board():
+    # 8x8
+    board = [
+        ["r", "n", "b", "q", "k", "b", "n", "r"],
+        ["p", "p", "p", "p", "p", "p", "p", "p"],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        ["P", "P", "P", "P", "P", "P", "P", "P"],
+        ["R", "N", "B", "Q", "K", "B", "N", "R"],
+    ]
+    return board
+
+
 @app.route("/")
 def home():
-    return "hello"
+    return render_template("index.html", board=init_board())
 
 
 if __name__ == "__main__":
