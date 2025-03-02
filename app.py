88d77ef948b3fc9a3aa91f51fe8379161102695e
pullasuti87 <pullasuti87@gmail.com>
2025-03-02 20:07:02 +0000

    move_works

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-
-def init_board():
-    # 8x8
-    board = [
+# 8x8
+board = [
         ["r", "n", "b", "q", "k", "b", "n", "r"],
         ["p", "p", "p", "p", "p", "p", "p", "p"],
         [" ", " ", " ", " ", " ", " ", " ", " "],
@@ -14,18 +12,32 @@ def init_board():
         [" ", " ", " ", " ", " ", " ", " ", " "],
         ["P", "P", "P", "P", "P", "P", "P", "P"],
         ["R", "N", "B", "Q", "K", "B", "N", "R"],
-    ]
-    return board
+]
+
+
+def pos_to_index(pos):
+    row = 8 - int(pos[1])
+    col = ord(pos[0]) - ord('a')
+    return row, col
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", board=init_board(), current_player="white")
+    return render_template("index.html", board=board, current_player="white")
 
 
 @app.route("/move", methods=['POST'])
 def move():
-    return "HELLO"
+    global board
+
+    from_row, from_col = pos_to_index(request.form['from_pos'])
+    to_row, to_col = pos_to_index(request.form['to_pos'])
+
+    piece = board[from_row][from_col]
+    board[to_row][to_col] = piece
+    board[from_row][from_col] = " "
+
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
